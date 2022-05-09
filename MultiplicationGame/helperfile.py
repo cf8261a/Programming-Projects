b539a7c4fad8c18b3ca6bf7e783ab34bb52bec48
Christian Fernandes <cf8261a@icloud.com>
2022-05-08 20:41:56 -0400

    Updated the code for DB, added PopUp functionality

--- MultiplicationGame/helperfile.py
+++ MultiplicationGame/helperfile.py
@@ -10,26 +10,75 @@ def buildExpression():
     return left_op, right_op, expression
 
 
-def createTable():
-    conn = sqlite3.connect('gamedata.db')
-    c = conn.cursor()
+# def createTable():
+#     conn = sqlite3.connect('gamedata.db')
+#     c = conn.cursor()
 
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS gamedata 
-    (sessionDate TEXT,
-    sessionTime TEXT,
-    CorrectCount INTEGER,
-    IncorrectCount INTEGER)
-    """)
+#     c.execute("""
+#     CREATE TABLE IF NOT EXISTS gamedata
+#     (sessionDate TEXT,
+#     sessionTime TEXT,
+#     CorrectCount INTEGER,
+#     IncorrectCount INTEGER)
+#     """)
 
-    conn.commit()
-    conn.close()
+#     conn.commit()
+#     conn.close()
 
 
-def insertValue(cCount, iCount):
-    conn = sqlite3.connect('gamedata.db')
-    c = conn.cursor()
-    q = f"INSERT INTO gamedata VALUES (date(), time(), {cCount}, {iCount})"
-    c.execute(q)
-    conn.commit()
-    conn.close()
+# def insertValue(cCount, iCount):
+#     conn = sqlite3.connect('gamedata.db')
+#     c = conn.cursor()
+#     q = f"INSERT INTO gamedata VALUES (date(), time(), {cCount}, {iCount})"
+#     c.execute(q)
+#     conn.commit()
+#     conn.close()
+
+class BackendDB:
+
+    def createTable(self):
+        q = """
+        CREATE TABLE IF NOT EXISTS gamedata 
+        (sessionDate TEXT,
+        sessionTime TEXT,
+        CorrectCount INTEGER,
+        IncorrectCount INTEGER)
+        """
+        return q
+
+    def insertValue(self, cCount, iCount):
+        """Creates a query to insert values into the existing db"""
+
+        q = f"INSERT INTO gamedata VALUES (date(), time(), {cCount}, {iCount})"
+        return q
+
+    def getHighestScore(self):
+        q = """SELECT MAX(CorrectCount) FROM gamedata"""
+        return q
+
+    def getAvgScore(self):
+        q = """SELECT AVG(CorrectCount) FROM gamedata"""
+        return q
+
+    def invokeSQLite(self, cCount=None, iCount=None, how=None):
+        """Creates a connection to the gamedb"""
+
+        conn = sqlite3.connect('gamedata.db')
+        c = conn.cursor()
+
+        if how == 'CREATETABLE':
+            query = self.createTable()
+        elif how == 'INSERTVALUE':
+            query = self.insertValue(cCount, iCount)
+        elif how == 'HIGHSCORE':
+            query = self.getHighestScore()
+        elif how == 'AVGSCORE':
+            query = self.getAvgScore()
+
+        c.execute(query)
+        rows = c.fetchall()
+
+        conn.commit()
+        conn.close()
+        if how in ['HIGHSCORE', 'AVGSCORE']:
+            return rows[0][0]
--- MultiplicationGame/maingame.py
+++ MultiplicationGame/maingame.py
@@ -1,15 +1,17 @@
 import PySimpleGUI as sg
 import helperfile
+import time
 
+gameDB = helperfile.BackendDB()
 # Ensure the db is created
-helperfile.createTable()
+gameDB.invokeSQLite(how='CREATETABLE')
 
 sg.theme('SandyBeach')   # Add a touch of color
 # All the stuff inside your window.
 layout = [[sg.Text('Welcome', key='-TITLE-')],
           [sg.Text('EXPRESSION', key='-HASEXPRESSION-'),
            sg.InputText(key='-ANSWER-', do_not_clear=False)],
-          [sg.Button('End Game', key='-END-')]]
+          [sg.Button('Finish', key='-FINISH-'), sg.Button('Close', key='-END-')]]
 
 # Default font
 defaultfont = ('Helvetica', 24)
@@ -26,10 +28,21 @@ window['-HASEXPRESSION-'].update(mathExpression)
 numCorrect = 0
 numIncorrect = 0
 
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
 
     event, values = window.read()
+
+    if event == '-FINISH-':
+        window['-ANSWER-'].update(visible=False)
+        window['-HASEXPRESSION-'].update(visible=False)
+        highestscore = gameDB.invokeSQLite(how='HIGHSCORE')
+        avgscore = gameDB.invokeSQLite(how='AVGSCORE')
+        finalMsg = f'Game Over! \n {numCorrect} correct \n {numIncorrect} incorrect.\n Highest Score All Time: {highestscore} \n Average All Time Score {avgscore} \n Thanks for Playing!'
+        sg.Popup(finalMsg)
+        break
+
     if event == sg.WIN_CLOSED or event == '-END-':
         break
 
@@ -48,6 +61,6 @@ while True:
     left_op, right_op, mathExpression = ExpressionResults
     window['-HASEXPRESSION-'].update(mathExpression)
 
-helperfile.insertValue(numCorrect, numIncorrect)
+gameDB.invokeSQLite(cCount=numCorrect, iCount=numIncorrect, how='INSERTVALUE')
 
 window.close()
